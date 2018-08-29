3fd0b39b0265871746231bbb93cd3287df8300c3
Marcos O. Alves <eng.marcos.alves@gmail.com>
2018-08-29 20:59:14 -0300

    classe de dominio, dao e configuração do hibernate

---
+++ webservice/src/br/com/geekcode/webservices/exceptions/DAOException.java
@@ -0,0 +1,21 @@
+package br.com.geekcode.webservices.exceptions;
+
+public class DAOException extends RuntimeException {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8474052563741461935L;
+	
+	private int code;
+	
+	public DAOException(String message, int code) {
+		super(message);
+		this.code = code;
+	}
+	
+	public int getCode( ) {
+		return code;
+	}
+
+}
---
+++ webservice/src/br/com/geekcode/webservices/model/dao/JPAUtil.java
@@ -0,0 +1,29 @@
+package br.com.geekcode.webservices.model.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPAUtil {
+	
+	/**
+	 * Este atributo é privado e estático asseguramos que haverá apenas 1 atributo
+	 * deste tipo para toda a aplicação.
+	 */
+	private static EntityManagerFactory emf;
+	
+	/**
+	 * Este método é estático porém publico para que possamos acessar de qualquer lugar
+	 * sem precisar instanciar a classe JPAUtil.
+	 * Este método tem o objetivo de retornar o entityManager (interface que utilizamos
+	 * para nos comunicar com o banco de dados)
+	 * @return
+	 */
+	public static EntityManager getEntityManager() {
+		if (emf == null) {
+			emf = Persistence.createEntityManagerFactory("produtos");
+		}
+		return emf.createEntityManager();
+	}
+
+}
